c13469ade629d319e5757636d998a2ceb3f0d791
Sandeep05862 <ramsingh05862@gmail.com>
2025-07-30 21:19:53 +0530

    Added files for Railway deployment

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Sanbinance Bot is running!"
-
-if __name__ == '__main__':
-    app.run()
+print("Sanbinance_bot is running")
