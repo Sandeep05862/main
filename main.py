1877a38ff08df3bc50a68a6bf1b6b83826ab581a
Sandeep Kumar Awasthi <ramsingh05862@gmail.com>
2025-07-30 23:16:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import os
+import threading
+import time
 
+# अपना बॉट का कोड यहां लिखो
+def run_bot():
+    while True:
+        print("Sanbinance_bot is running")
+        # यहां ट्रेडिंग लॉजिक लगाओ
+        time.sleep(10)
+
+# Flask App
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "main is running!"
+    return 'Sanbinance_bot is running in background!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    bot_thread = threading.Thread(target=run_bot)
+    bot_thread.start()
+
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
